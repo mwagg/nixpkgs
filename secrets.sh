ff9b48bde6ef6eb5b78451410a93961e0d40f95c
mwagg <michael@guerillatactics.co.uk>
2021-10-14 16:22:07 +0100

    Add secret

--- secrets.sh
+++ secrets.sh
@@ -29,10 +29,11 @@ get_secret e93fa440-324a-419e-acd4-aa7300d8ed5d chocg8j9jn0ce67leh3u8my7ybp24d73
 
 get_secret 2b3bfe9d-ef76-4bba-9a27-a80000d36e86 wvb3nx6m5evq5qgk76li684hqv34zksi ~/.ssh/id_rsa
 get_secret 2b3bfe9d-ef76-4bba-9a27-a80000d36e86 2gnovdize9kfq9maa0g1oenj9fn823wt ~/.ssh/id_rsa.pub
+get_secret 2b3bfe9d-ef76-4bba-9a27-a80000d36e86 w6zub46sfg8be24gohor5mnakx0m6kn7 ~/.ssh/config
 
 mkdir -p ~/.config/aws
-get_secret 38bbeb4e-5e9f-4e94-8cb6-aa4700e960d1 3hdt0ik26ch8m8qf22g797utceak2qa6 ~/.config/aws/config
-get_secret 38bbeb4e-5e9f-4e94-8cb6-aa4700e960d1 ihy2nzu81t823penxn2wx6wd9lc36jjg ~/.config/aws/credentials
+get_secret 38bbeb4e-5e9f-4e94-8cb6-aa4700e960d1 3hdt0ik26ch8m8qf22g797utceak2qa6 ~/.aws/config
+get_secret 38bbeb4e-5e9f-4e94-8cb6-aa4700e960d1 ihy2nzu81t823penxn2wx6wd9lc36jjg ~/.aws/credentials
 
 get_secret e0c16ca1-0baf-49a9-90d5-aa8e00a625c3 lwwi495agi0wh66uk2xd5wbg9yyp7mip ~/.terraformrc
 
